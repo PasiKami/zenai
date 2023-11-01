0aa991c7da46bc47fb20a3b04eaa395f748274bb
jiabing.han <1428727144@qq.com>
2023-11-01 16:06:14 +0800

    upload file

---
+++ pages/模型和价格.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("模型和价格")
+st.title("")
+st.markdown("""##### 现支持gpt-3.5-turbo, gpt-4, 价格如下:""")
+
+st.markdown("[![Click me](app/static/price.png)](https://streamlit.io)")
+
+st.markdown("""##### 1k tokens ≈ 552个汉字, 费率与官方一致，8毛一刀，相当于官方价格的1折 ！(快涨价了, 先到先得!)""")
---
+++ 简介.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("ZEN AI 介绍")
+st.title("")
+st.markdown("""
+- ##### 我们致力于降低使用AI的门槛和费用，提高速度和稳定。
+- ##### 此平台主要面向任何希望使用AI的个人用户。
+- #### 有高并发需求,每日用量达20刀的用户请直接私聊[群主](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=CjK9AAgGMV3ZjOiKkYfzlaYqvA6RoWuu&authKey=ual5BYkxY%2Fk0H%2FgbTBinUD%2BUklGZKpYWx4iTr6AhkG1uKhYReVtsXOUbHgGD6U%2F8&noverify=0&group_code=148190284)开启高速通道!
+
+""")
